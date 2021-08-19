c26113e9fb3a50a7737e08067020397270abda4f
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 16:53:13 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,16 @@
+// number
+// integer : 1 2 3 4 5 6 145
+var watchPrice = 120;
+watchPrice = 125;
+
+// string
+var userName = "Jahangir Alom Minto";
+
+// boolean
+var isExpensive = true;
+
+// integer : 1 2 3 4 5 6 145
+// float : 2.714  5.98745  3.14
+var howPoint = 2.7;
+
+
