ef617f8a65fc27867321b4f45a2bbc610ad4538e
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 18:55:41 +0600

    js: to add first in the array- use unshift() method, to remove from the first.... use shift() method, to add in the last of an array use ... push() method;  and to remove from the last of an array use... pop() method

---
+++ push-pop.js
@@ -0,0 +1,68 @@
+// add elements to an array
+var lastBench = ['kalam', 'balam', 'salam'];
+lastBench.push('jalam');
+lastBench.push('palam');
+lastBench.push('malam');
+
+console.log(lastBench);
+
+
+var friendsAge = [11, 13, 17, 12];
+console.log(friendsAge);
+
+
+var friendsAge = [11, 13, 17, 12];
+friendsAge.push(21);
+friendsAge.push(28);
+console.log(friendsAge);
+
+
+// remove elements from an array
+friendsAge.pop();
+console.log(friendsAge);
+
+// remove elements from an array
+friendsAge.pop();
+friendsAge.pop();
+console.log(friendsAge);
+
+
+// remove elements from an array
+
+friendsAge.pop();
+console.log(friendsAge);
+
+
+// remove elements from an array
+var lastItem = friendsAge.pop();
+// friendsAge.pop();
+console.log(friendsAge);
+console.log(lastItem);
+
+
+
+// ***** Remove the first item of an array: ******
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+console.log(fruits);
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);
+
+
+//The Array.unshift() method adds new items to the beginning of an array:
+const fruitsNatural = ["Cocunut", "Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon", "Pineapple");
+console.log(fruitsNatural);
+
+
+const fruitsNaturalMore = ["Cocunut", "Banana", "Orange", "Apple", "Mango"];
+fruitsNaturalMore.unshift("Lemon", "Pineapple");
+console.log(fruitsNaturalMore);
+
+
+
+
