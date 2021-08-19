b8ee61018676789c70a6dc8b093e7d317181edcd
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 19:23:23 +0600

    comparison.js file done with practice

---
+++ comparison.js
@@ -0,0 +1,29 @@
+// greater than
+console.log( 5 > 6 ); 
+
+// less than
+console.log( 5 < 6 ); 
+
+// both are equals : not : false
+console.log( 5 == 6 );
+
+// both are equals : yes : true
+console.log( 6 == 6 ); 
+
+// 5 is not equal 6 : yes : true
+console.log( 5 != 6 ); 
+
+// 6 is not equal 6 : yes : false
+console.log( 6 != 6 ); 
+
+var myLove = 100;
+var herLove = 99;
+console.log(myLove > herLove);
+console.log(myLove < herLove);
+console.log(myLove == herLove);
+console.log(myLove != herLove);
+
+
+// console.log(studyGood && goodPerson);
+// console.log(isBanker || goodResult);
+
