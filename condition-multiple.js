e8b86c83a8c78c29142331719df2b5b2d58b3c7f
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 20:38:06 +0600

    conditon-multiple in if else with logical and as well as logical or operator

---
+++ condition-multiple.js
@@ -0,0 +1,132 @@
+var gotJob = true;
+var moneySaved = 150000;
+
+if (gotJob == true) {
+    console.log('lets marry')
+}
+else {
+    console.log('I have no money now');
+}
+
+
+var gotJob1 = false;
+
+if (gotJob1 == true) {
+    console.log('lets marry')
+}
+else {
+    console.log('I have no money now');
+}
+
+
+var gotJob2 = true;
+var moneySaved = 150000;
+if (gotJob2 == true && moneySaved > 200000) {
+    console.log('Cholo biye kore feli')
+}
+else {
+    console.log('Tor kopale biye nai!');
+}
+
+
+var gotJob2 = true;
+var moneySaved = 250000;
+if (gotJob2 == true && moneySaved > 200000) {
+    console.log('Cholo biye kore feli')
+}
+else {
+    console.log('Tor kopale biye nai!');
+}
+
+
+var haveJob = true;
+var haveMoney = 250000;
+var haveFlat = false;
+if (haveJob == true && haveMoney > 200000 && haveFlat == true) {
+    console.log('you are perfect now');
+}
+else {
+    console.log('You are not perfect now!')
+}
+
+
+var haveJob = true;
+var haveMoney = 250000;
+var haveFlat = false;
+if (haveJob == true && haveMoney > 200000 && haveFlat == false) {
+    console.log('you are perfect now');
+}
+else {
+    console.log('You are not perfect now!')
+}
+
+
+var haveJobMe = false;
+var haveMoneyMe = 250000;
+var haveFlatMe = true;
+if (haveJobMe == true || haveMoneyMe > 200000) {
+    console.log('you are Josh now');
+}
+else {
+    console.log('You are not Josh now!')
+}
+
+
+var haveJobMe = false;
+var haveMoneyMe = 50000;
+var haveFlatMe = true;
+if (haveJobMe == true || haveMoneyMe > 200000) {
+    console.log('you are Josh now');
+}
+else {
+    console.log('You are not Josh now!')
+}
+
+
+var okJob = false;
+var okMoney = 50000;
+var okFlat = false;
+var okHouse = false;
+if (okJob == true && okMoney > 200000 && okFlat == true) {
+    console.log('You are winner to marry')
+}
+else {
+    console.log('You are looser to marry')
+}
+
+
+var okJob = true;
+var okMoney = 50000;
+var okFlat = false;
+var okHouse = false;
+if (okJob == true && okMoney > 200000 && okFlat == true) {
+    console.log('You are winner to marry')
+}
+else {
+    console.log('You are looser to marry')
+}
+
+
+var okJob = true;
+var okMoney = 250000;
+var okFlat = false;
+var okHouse = true;
+
+if (okJob == true || okMoney > 200000 || okFlat == true || okHouse == true) {
+    console.log('You are winner to marry')
+}
+else {
+    console.log('You are looser to marry')
+}
+
+
+var okJob = false;
+var okMoney = 50000;
+var okFlat = false;
+var okHouse = false;
+if (okJob == true && okMoney > 200000 && okFlat == true) {
+    console.log('You are winner to marry')
+}
+else {
+    console.log('You are looser to marry')
+}
