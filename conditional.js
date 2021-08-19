de63fce3e971da19547d583365c4dc154e2dd34e
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 19:51:11 +0600

    learned and practiced condtional.js file properly

---
+++ conditional.js
@@ -0,0 +1,75 @@
+var isFoodReady = true;
+var isWaterReady = false;
+// jodi food ready hoy taile ami khamu
+// if food is ready, I will eat.
+// if food is ready == true
+
+ /* 
+ if (condition) {
+
+} 
+*/
+
+if (isFoodReady == true) {
+    console.log('Mama!! I will eat now.');
+}
+
+if (isWaterReady == false) {
+    console.log('Mama make water ready Otherwise!! I will not eat now.');
+}
+
+
+if (isWaterReady == true) {
+    console.log('Mama!! I will not eat now.');
+}
+
+
+var iphonePrice = 70000;
+var myBudget = 9500;
+if (iphonePrice < myBudget) {
+    console.log('I am not able to purchase the iPhone!');
+}
+
+var iphonePrice1 = 70000;
+var myBudget1 = 120000;
+if (iphonePrice1 < myBudget1) {
+    console.log('Now, I am able to purchase the iPhone!');
+}
+
+var iphonePrice = 70000;
+var myBudget = 9500;
+if (iphonePrice > myBudget) {
+    console.log('My Samsung phone is the best!');
+}
+
+
+// condition with if else
+
+/* if (condition) {
+    //output code goes here
+}
+else {
+    //output code goes here
+} */
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log('Yes!! Murgir run khamu ar haddi chibamu');
+}
+else {
+    console.log(' oh no!! smashed potato with lentil soup!');
+}
+
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if (chickenPrice < myMoney) {
+    console.log('Yes!! Murgir run khamu ar haddi chibamu');
+}
+else {
+    console.log(' oh no!! smashed potato with lentil soup!');
+}
+
